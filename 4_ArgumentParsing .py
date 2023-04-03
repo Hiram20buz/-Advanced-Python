d60bc08eb097213c4d8ca3fde1644a75c224df45
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-04-03 11:33:35 -0700

    Update 4_ArgumentParsing .py

--- 4_ArgumentParsing .py
+++ 4_ArgumentParsing .py
@@ -5,11 +5,28 @@ print(sys.argv[0])
 print(sys.argv[1])
 '''
 
-# Usage: main.py Filename message
-
+# Usage: main.py Filename
+'''
 filename = sys.argv[1]
 message = sys.argv[2]
 
 with open(filename,'w+') as f:
     f.write(message)
+'''
 
+import getopt
+
+filename="test.txt"
+message="Hello"
+
+opts,args=getopt.getopt(sys.argv[1:],"f:m",["filename","message"])
+
+for opt,arg in opts:
+    if opt == '-f':
+        filename=arg
+    if opt== '-m':
+        message = arg
+        
+with open(filename,'w+') as f:
+    f.write(message)
+    
