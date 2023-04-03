25da49c0f8c3265bb17b615adfe316ad768e0dad
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-04-03 10:42:54 -0700

    Update 2_Decorators.py

--- 2_Decorators.py
+++ 2_Decorators.py
@@ -40,3 +40,22 @@ def hello(Person):
   
 hello("Mike")
 '''
+
+# Practical Example 1 -Logging
+def logged(function):
+    def wrapper(*args,**kwargs):
+        value=function(*args,**kwargs)
+        with open('logfile.txt','a+') as f:
+            fname=function.__name__
+            print(f"{fname} returned value {value}")
+            f.write(f"{fname} returned value {value}\n")
+        return value
+        
+    return wrapper
+ 
+@logged       
+def add(x,y):
+    return x+y
+    
+print(add(10,20))
+
