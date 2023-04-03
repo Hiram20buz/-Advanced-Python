88d95033ee1d70133c94c3e502f3d1d4a9f86a0c
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-04-03 10:55:18 -0700

    Update 2_Decorators.py

--- 2_Decorators.py
+++ 2_Decorators.py
@@ -42,6 +42,7 @@ hello("Mike")
 '''
 
 # Practical Example 1 -Logging
+'''
 def logged(function):
     def wrapper(*args,**kwargs):
         value=function(*args,**kwargs)
@@ -58,4 +59,29 @@ def add(x,y):
     return x+y
     
 print(add(10,20))
+'''
 
+# Practical Example 2 -Timing
+
+import time
+def timed(function):
+    def wrapper(*args,**kwargs):
+        before=time.time()
+        value=function(*args,**kwargs)
+        after=time.time()
+        fname=function.__name__
+        print(f"{fname} took {after-before} seconds to execute!")
+        return value
+        
+    return wrapper
+
+ 
+@timed
+def myfunction(x):
+    result=1
+    for i in range(1,x):
+        result *= i
+        
+    return result
+    
+myfunction(10)
